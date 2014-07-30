99501cf7213315ac6184e3b6e97b448a996a8df9
welshej <welshej@gmail.com>
2014-07-30 13:18:50 -0400

    Switched out to Newer Videos

--- videos.php
+++ videos.php
@@ -27,53 +27,55 @@ NOTE: To add a video, copy the following code. Paste the link to your video in b
         		</div>
 */
 
-        		<div class="half youtube-vid inner">
-        			<h2>Crash Test</h2>
-        			<div class="divider red-divider"></div>
-        			<div class="vidwrapper">
-        				<iframe width="560" height="315" src="//www.youtube.com/embed/mS48z5N4Oas" frameborder="0" allowfullscreen></iframe>
-        			</div>
-        		</div>
+<div class="half youtube-vid inner">
+    <h2>Flying in the Rain</h2>
+    <div class="divider red-divider"></div>
+    <div class="vidwrapper">
+        <iframe width="560" height="315" src="https://www.youtube.com/watch?v=tJ9OSGyn1nU&list=PLAzeszoOIDGR42BIjEXvFyPudXll2lN2r&index=6" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
 
-        		<div class="half youtube-vid inner">
-        			<h2>Third Generation DJI</h2>
-        			<div class="divider red-divider"></div>
-        			<div class="vidwrapper">
-        				<iframe width="640" height="360" src="//www.youtube.com/embed/M2V3zCEztmg?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
-        			</div>
-        		</div>
 
-        		<div class="half youtube-vid inner">
-        			<h2>Wobbling Flight</h2>
-        			<div class="divider red-divider"></div>
-        			<div class="vidwrapper">
-        				<iframe width="640" height="360" src="//www.youtube.com/embed/93vyDoayd_E?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
-        			</div>
-        		</div>
 
-        		<div class="half youtube-vid inner">
-        			<h2>Crash Test</h2>
-        			<div class="divider red-divider"></div>
-        			<div class="vidwrapper">
-        				<iframe width="560" height="315" src="//www.youtube.com/embed/mS48z5N4Oas" frameborder="0" allowfullscreen></iframe>
-        			</div>
-        		</div>
+<div class="half youtube-vid inner">
+    <h2>Weatherization Process Explained</h2>
+    <div class="divider red-divider"></div>
+    <div class="vidwrapper">
+        <iframe width="560" height="315" src="https://www.youtube.com/watch?v=reHHEuZCW-I&list=PLAzeszoOIDGR42BIjEXvFyPudXll2lN2r&index=5" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
 
-        		<div class="half youtube-vid inner">
-        			<h2>Third Generation DJI</h2>
-        			<div class="divider red-divider"></div>
-        			<div class="vidwrapper">
-        				<iframe width="640" height="360" src="//www.youtube.com/embed/M2V3zCEztmg?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
-        			</div>
-        		</div>
 
-        		<div class="half youtube-vid inner">
-        			<h2>Wobbling Flight</h2>
-        			<div class="divider red-divider"></div>
-        			<div class="vidwrapper">
-        				<iframe width="640" height="360" src="//www.youtube.com/embed/93vyDoayd_E?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
-        			</div>
-        		</div>
+
+<div class="half youtube-vid inner">
+    <h2>Quad Flight in Rain with Exposed Electronics</h2>
+    <div class="divider red-divider"></div>
+    <div class="vidwrapper">
+        <iframe width="560" height="315" src="https://www.youtube.com/watch?v=Qwlc-bg8vTs&list=PLAzeszoOIDGR42BIjEXvFyPudXll2lN2r&index=3" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
+
+
+
+<div class="half youtube-vid inner">
+    <h2>Maiden Voyage</h2>
+    <div class="divider red-divider"></div>
+    <div class="vidwrapper">
+        <iframe width="560" height="315" src="https://www.youtube.com/watch?v=7LnCqbUfpB0&list=PLAzeszoOIDGR42BIjEXvFyPudXll2lN2r&index=2" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
+
+
+
+<div class="half youtube-vid inner">
+    <h2>Maiden Voyage 2</h2>
+    <div class="divider red-divider"></div>
+    <div class="vidwrapper">
+        <iframe width="560" height="315" src="https://www.youtube.com/watch?v=gcfAL5Hj_Bc&list=PLAzeszoOIDGR42BIjEXvFyPudXll2lN2r&index=4" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
+
+
         		
     
         	</div>
